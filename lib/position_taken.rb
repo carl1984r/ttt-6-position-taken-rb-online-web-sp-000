7edad5fb3b23259bc41d8a6703adfbb8caf0f8ae
Bry Cox <carl.cc@gmail.com>
2018-12-29 21:05:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 put require "pry"
 
-board = ["binding.pry", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+binding.pry
 
 def position_taken?(board, index)
   if (index != "X" || index != "O")
