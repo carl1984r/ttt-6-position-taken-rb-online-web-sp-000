2232f8c13160f03f6fee9a5c2f643c46b7ae4180
Bry Cox <carl.cc@gmail.com>
2018-12-29 20:41:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, index)
-  if (index != "X" && index != "O")
+  if (index != "X" || index != "O")
       return false
-    else
-      index == "X" && index == "O"
-      return true
-    end
+  end
 end
